e222bc27b3d3266a5406b2eb2bf0dae0b21dfe96
Louis Jaume <jaume.l@etoncollege.org.uk>
2023-02-04 10:50:42 +0000

    Initial Commit

---
+++ CommentCardWriterApp/CommentCardWriterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterAppApp.swift
+//  CommentCardWriterApp
+//
+//  Created by Jaume, Louis (NA) on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommentCardWriterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CommentCardWriterApp
+//
+//  Created by Jaume, Louis (NA) on 04/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
